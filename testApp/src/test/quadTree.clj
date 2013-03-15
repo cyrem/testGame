67511e52388f08eb95aca96897258f7aa7ac6c5d
mathias juergens <m.juerg@online.de>
2013-03-15 06:37:14 +0100

    leaf / node

--- testApp/src/test/quadTree.clj
+++ testApp/src/test/quadTree.clj
@@ -1,36 +1,8 @@
 (ns test.quadTree)
 
 
-(defn rectIntersect [[minX minY maxX maxY] [minRectX minRectY maxRectX maxRectY]] 
-  (not (or (> minRectX maxX)
-           (< maxRectX minX)
-           (> minRectY maxY)
-           (< maxRectY minY)))) 
 
-(defn selectSubNode [[midX midY] x y]
-  (cond
-    (and (< x midX) (< y midY)) :sw
-    (and (< x midX) (> y midY)) :nw
-    (and (> x midX) (< y midY)) :se
-    (and (> x midX) (> y midY)) :ne))
-
-(defrecord node [bound v nw ne sw se])
-
-(comment (defn quadIns [node x y v]
-  (cond
-    (empty? node) (node. x y v nil nil nil nil)
-    :else (let [selectedNode (selectSubNode node x y)]
-            (assoc node selectedNode (quadIns (get node selectedNode) x y v)) ))))
-
-
-(def dadum [{:x 4 :y 35 :v "sffsfda copy"}
-            {:x 5 :y 40 :v "sdfaf"}
-            {:x 11 :y 55 :v "sdfaf1"}
-            {:x 99 :y 22 :v "sdfaf2"}
-            {:x 100 :y 100 :v "sdfaf3"}
-            ])
-
-
+(comment 
 (defn createQuad [items depth [^int minX ^int minY ^int maxX ^int maxY]]
   (when (> depth 0)
     (let
@@ -53,4 +25,60 @@
            (createQuad (:se filtered) (dec depth) [midY midY maxX maxY])))))
 
 (def dadi (createQuad dadum 3 [1 1 100 100]))
+)
+
+(def dadum [{:x 4 :y 35 :v "sffsfda copy"}
+            {:x 5 :y 40 :v "sdfaf"}
+            {:x 11 :y 55 :v "sdfaf1"}
+            {:x 99 :y 22 :v "sdfaf2"}
+            {:x 100 :y 100 :v "sdfaf3"}])
+
+
+(defn rectIntersect [[minX minY maxX maxY] [minRectX minRectY maxRectX maxRectY]] 
+  (not (or (> minRectX maxX)
+           (< maxRectX minX)
+           (> minRectY maxY)
+           (< maxRectY minY)))) 
+
+(defn selectSubNode [[midX midY] x y]
+  (cond
+    (and (< x midX) (< y midY)) :sw
+    (and (< x midX) (> y midY)) :nw
+    (and (> x midX) (< y midY)) :se
+    (and (> x midX) (> y midY)) :ne))
+
+(defrecord node [bound nw ne sw se])
+(defrecord leaf [vals])
+(defrecord quadTree [root bounds maxDepth maxItems])
+
+;[]
+(defn createQuad [bounds maxD maxI]
+  (quadTree. (node. bounds nil nil nil nil) bounds maxD maxI))
+
+(def test1 (quadTree. (node. [800 800] nil nil nil nil) [800 800] 5 5))
+
+(defn quadIns [tree vals]
+  (let [[^int minX ^int minY ^int maxX ^int maxY] (:bounds tree)
+        midX (int (/ (+ minX maxX) 2))
+        midY (int (/ (+ minY maxY) 2))
+        midB [midX midY]
+        [val & rest] vals]
+    
+    (cond
+      (instance? leaf selected) (do
+                                  )
+      (instance? node selected) (do
+                                  )
+      ))
+    
+    
+    )
+  
+
+)
+
+
+
+
+
 
