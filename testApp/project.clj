a3ecde94e24eb150b1cb5b9a6727f7d63f15260f
mathias juergens <m.juerg@online.de>
2013-04-06 17:01:22 +0200

    -

--- testApp/project.clj
+++ testApp/project.clj
@@ -1,9 +1,6 @@
-(defproject test "0.1.0-SNAPSHOT"
+(defproject nova "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 ;[korma "0.3.0-beta14"]
-                 [org.xerial/sqlite-jdbc "3.7.2"]
-                 [seesaw "1.4.2"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])
